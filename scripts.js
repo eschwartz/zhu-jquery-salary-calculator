8e5f75c9c0ce017b8da49e219c8d5f264a661ffd
Edan Schwartz <edanschwartz@gmail.com>
2020-12-14 09:53:11 -0600

    Grab employee from DOM inputs

--- scripts.js
+++ scripts.js
@@ -2,4 +2,25 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('So ready!');
+
+  $('#newEmployeeBtn').on('click', addEmployee);
+}
+
+function addEmployee() {
+  console.log('addEmployee was clicked');
+
+  let firstName = $('#firstNameInput').val();
+  let lastName = $('#lastNameInput').val();
+  let id = $('#idInput').val();
+  let title = $('#titleInput').val();
+  let salary = $('#salaryInput').val();
+  
+  let employee = {
+    firstName: firstName,
+    lastName: lastName,
+    id: id,
+    title: title,
+    salary: salary
+  };
+  console.log('employee to add is', employee);
 }
