59414c34ed3ee63aa53caaff0de53ee431ad0bbf
Edan Schwartz <edanschwartz@gmail.com>
2020-12-14 09:56:30 -0600

    comment my code for fun and profit

--- scripts.js
+++ scripts.js
@@ -3,18 +3,22 @@ $(document).ready(onReady);
 function onReady() {
   console.log('So ready!');
 
+  // Handle Submit button click
   $('#newEmployeeBtn').on('click', addEmployee);
 }
 
 function addEmployee() {
   console.log('addEmployee was clicked');
 
+  // Grab employee info from the DOM
   let firstName = $('#firstNameInput').val();
   let lastName = $('#lastNameInput').val();
   let id = $('#idInput').val();
   let title = $('#titleInput').val();
   let salary = $('#salaryInput').val();
   
+  // Wrap the employee up in a
+  // nice little object
   let employee = {
     firstName: firstName,
     lastName: lastName,
