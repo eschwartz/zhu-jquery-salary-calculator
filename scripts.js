45040a66fed3ed0047c7feadcd74f865aee83631
Edan Schwartz <edanschwartz@gmail.com>
2020-12-14 11:54:31 -0600

    Some light refactoring

--- scripts.js
+++ scripts.js
@@ -14,38 +14,18 @@ function onReady() {
 // Track total monthly costs for all employees
 let totalMonthlyCost = 0;
 
+/**
+ * Handle "Submit" button to add an employee
+ * Renders the employee to the table
+ * Updates the monthly cost
+ * Makes cost red if >$20k
+ */
 function addEmployee() {
   console.log('addEmployee was clicked');
 
-  // Grab employee info from the DOM
-  let firstName = $('#firstNameInput').val();
-  let lastName = $('#lastNameInput').val();
-  let id = $('#idInput').val();
-  let title = $('#titleInput').val();
-  let salary = $('#salaryInput').val();
-  
-  // Wrap the employee up in a
-  // nice little object
-  let employee = {
-    firstName: firstName,
-    lastName: lastName,
-    id: id,
-    title: title,
-    salary: Number(salary)
-  };
-  console.log('employee to add is', employee);
+  let employee = getEmployeeFromDOM();
 
-  // Append the employee to the DOM
-  $('#employeeTable').append(`
-    <tr>
-      <td>${employee.firstName}</td>
-      <td>${employee.lastName}</td>
-      <td>${employee.id}</td>
-      <td>${employee.title}</td>
-      <td>$${employee.salary}</td>
-      <td><button class="deleteBtn">Delete</button></td>
-    </tr>
-  `);
+  renderEmployee(employee);
 
   // Clear input fields
   $('#employeeForm input').val('');
@@ -61,11 +41,53 @@ function addEmployee() {
   if (totalMonthlyCost > 20000) {
     $('#totalMonthlyCost').addClass('overBudget');
   }
+  // Reset background color, if <$20k
   else {
     $('#totalMonthlyCost').removeClass('overBudget');
   }
 }
 
+function getEmployeeFromDOM() {
+  // Grab employee info from the DOM
+  let firstName = $('#firstNameInput').val();
+  let lastName = $('#lastNameInput').val();
+  let id = $('#idInput').val();
+  let title = $('#titleInput').val();
+  let salary = $('#salaryInput').val();
+
+  // Wrap the employee up in a
+  // nice little object
+  let employee = {
+    firstName: firstName,
+    lastName: lastName,
+    id: id,
+    title: title,
+    salary: Number(salary)
+  };
+  console.log('employee to add is', employee);
+
+  return employee;
+}
+
+function renderEmployee(employee) {
+// Append the employee to the DOM
+$('#employeeTable').append(`
+  <tr>
+    <td>${employee.firstName}</td>
+    <td>${employee.lastName}</td>
+    <td>${employee.id}</td>
+    <td>${employee.title}</td>
+    <td>$${employee.salary}</td>
+    <td><button class="deleteBtn">Delete</button></td>
+  </tr>
+  `);
+}
+
+/**
+ * Delete employee from the DOM
+ * 
+ * NOTE: does not update total monthly costs
+ */
 function deleteEmployee() {
   console.log('deleting an employee', $(this));
 
